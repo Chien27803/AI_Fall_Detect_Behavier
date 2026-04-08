c38596c72d90bba90a12d0ce868644f37bcfafdb
Nhu Dinh Chien <nhudinhchien12345@gmail.com>
2026-04-09 01:24:39 +0700

    cc

--- ai/src/make_data.py
+++ ai/src/make_data.py
@@ -5,11 +5,11 @@ import os
 
 # ====== CONFIG ======
 label = "ADL"   # đổi label khi thu hành động khác
-subject_name = "subject6"
+subject_name = "subject9"
 no_of_frames = 210      # số frame cho 1 sample (chuẩn LSTM)
 
 base_save_path = "dataset"
-subfolder_name = "train_s6"
+subfolder_name = "train_s9"
 save_path = os.path.join(base_save_path, subfolder_name)
 
 os.makedirs(save_path, exist_ok=True)
